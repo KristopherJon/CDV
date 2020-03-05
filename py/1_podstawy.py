d814642e2fe412bcf861412ccd537f3ddfa6b90c
Krzysztof Jonczyk <kjonczyk@edu.cdv.pl>
2020-03-05 17:21:29 +0100

    Całe zajęcia

---
+++ py/1_podstawy.py
@@ -0,0 +1,57 @@
+print("Janusz")
+
+'''komentarz blokowy
+
+coś tak
+
+
+
+#jakiśinny komentarz
+
+potega = 2**10
+print(potega)
+
+
+#pobranie danych z klawiatury
+name=input("podaj imie ")
+surname=input("podaj nazwisko ")
+
+print("imie: " + name + " nazwisko: " + surname)
+
+
+print("podaj swoj wiek: ", end="")
+age=input()
+
+age1=20
+print("twoj wiek:", age1, "lat")
+
+
+#########
+
+surname = "kowalski"
+
+firstChar = surname[0]
+print(firstChar)
+
+length=len(surname)
+
+print(length)
+
+
+lastChar = surname[len(surname) -1 ]
+print(lastChar)
+
+
+############
+'''
+x = "5"
+print(type(x))
+
+x = int(x)
+print(type(x))
+
+y=4
+print(type(y))
+y=y/2
+print(type(y))
+
